536641fb5d5f828a34846e7af0add432d7393b4a
AdamaCoulbaly <100707320+AdamaCoulbaly@users.noreply.github.com>
2022-04-13 15:15:16 -0600

    Adama

--- PixelRaffle/app/src/main/java/com/example/pixelraffle/ui/screens/adama/ContentScreens.kt
+++ PixelRaffle/app/src/main/java/com/example/pixelraffle/ui/screens/adama/ContentScreens.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -16,7 +17,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -64,7 +67,7 @@ fun RegisterPage(){
         Row(horizontalArrangement = Arrangement.Center,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(start = 2.dp, top = 0.dp, end = 2.dp, bottom = 10.dp)
+                        .padding(start = 2.dp, top = 0.dp, end = 2.dp, bottom = 20.dp)
             ){
                 Image( 
                     painterResource(id = R.drawable.pixelrafflelogo),
@@ -91,6 +94,33 @@ fun RegisterPage(){
             Text(text="Pixel Raffle", color = graySurface, fontSize = 18.sp)
         }
 
+        Spacer(modifier = Modifier.padding(5.dp))
+        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()){
+            //User Fisrt Name
+            OutlinedTextField(value = firstName.value, onValueChange = {firstName.value=it},
+                label = {Text(text="First Name", color = Color.Black, style = TextStyle(letterSpacing = TextUnit.Unspecified),
+                    fontSize = TextUnit.Unspecified)},
+
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(0.8f))
+
+            //User Fisrt Name
+            OutlinedTextField(value = lastName.value, onValueChange = {lastName.value=it},
+                label = {Text(text="Last Name", color = Color.Black, style = TextStyle(letterSpacing = TextUnit.Unspecified),
+                    fontSize = TextUnit.Unspecified)},
+
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(0.8f))
+
+            //Email Addres textfield field
+            OutlinedTextField(value = email.value, onValueChange = {email.value=it},
+                label = {Text(text="Email Address", color = Color.Black, style = TextStyle(letterSpacing = TextUnit.Unspecified),
+                    fontSize = TextUnit.Unspecified)},
+
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(0.8f))
+
+        }
     }
 
 
