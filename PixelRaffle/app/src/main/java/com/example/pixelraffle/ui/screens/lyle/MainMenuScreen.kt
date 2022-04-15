f6b728ec72190caa25609a0475751c5765c78cdb
Lyle <lylelopez03@gmail.com>
2022-04-15 15:43:38 -0700

    update

--- PixelRaffle/app/src/main/java/com/example/pixelraffle/ui/screens/lyle/MainMenuScreen.kt
+++ PixelRaffle/app/src/main/java/com/example/pixelraffle/ui/screens/lyle/MainMenuScreen.kt
@@ -7,12 +7,16 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -38,42 +42,95 @@ import com.example.pixelraffle.ui.theme.*
 
 @Composable
 fun MainMenuScreen(navController: NavController){
+    val RoomID = rememberSaveable{ mutableStateOf("") }
 
-    Scaffold(modifier = Modifier.fillMaxSize(),
-        bottomBar = {BottomNavigationBar(navController)}
-        ) {
+    Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {BottomNavigationBar(navController)}) {
 
         Surface(modifier = Modifier.fillMaxSize()) {
-          //  Image(painter = painterResource(id = R.drawable.mnbase_02), contentDescription = "",alpha = .13f,contentScale = ContentScale.FillBounds)
-
-
             Row(modifier = Modifier
                 .fillMaxWidth()
                 .offset(y = 80.dp),horizontalArrangement = Arrangement.Center) {
                 Text(text = "WELCOME:   .",textAlign = TextAlign.Center,fontFamily = FontFamily.Default,fontSize = 25.sp,fontWeight = FontWeight.Bold)
             }
-            
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .offset(y = -150.dp),verticalAlignment = Alignment.CenterVertically,horizontalArrangement = Arrangement.SpaceEvenly) {
-                
-                FloatingActionButton(onClick = { navController.navigate(NavScreens.CreateRoom.route) },Modifier.size(120.dp,120.dp),shape = MaterialTheme.shapes.medium,backgroundColor = MaterialTheme.colors.primary) {
-                    Text(text = "Create Room",color = Color.White)
-                    
+
+            Column(modifier=Modifier.fillMaxWidth().offset(y=200.dp)/*,verticalArrangement = Arrangement.SpaceEvenly*/) {
+
+
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(y = 10.dp),verticalAlignment = Alignment.CenterVertically,horizontalArrangement = Arrangement.SpaceEvenly) {
+
+                    Button(
+                        onClick = { navController.navigate(NavScreens.CreateRoom.route) },
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .width(350.dp),
+                        shape = CircleShape,/*border = BorderStroke(2.dp,color = Color.White)*/
+                    ) {
+                        Text(text = "Create Room", modifier = Modifier.padding(2.dp), color = Color.White)
+
+                    }
+                }
+                //Spacer(modifier = Modifier.height(50.dp))
+
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(y = 30.dp),verticalAlignment = Alignment.CenterVertically,horizontalArrangement = Arrangement.SpaceEvenly) {
+
+                    OutlinedTextField(value = RoomID.value, onValueChange = {RoomID.value=it},
+                        label = {Text(text = buildAnnotatedString {
+                            append("Room ID")
+                            addStyle(
+                                style = SpanStyle(
+                                    color = Color.Red,
+                                ),
+                                start = 0,
+                                end = 6
+                            )
+                            addStyle(
+                                style = SpanStyle(
+                                    color = Color.Black,
+                                ),
+                                start = 6,
+                                end = 13
+                            )
+                        },
+                            fontSize = 13.sp,
+                        )},
+
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth(0.9f)
+                    )
+
+                }
+                //Spacer(modifier = Modifier.height(300.dp))
+
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(y = 40.dp),verticalAlignment = Alignment.CenterVertically,horizontalArrangement = Arrangement.SpaceEvenly) {
+
+                    Button(onClick = {  navController.navigate(NavScreens.Room.route) },modifier= Modifier
+                        .padding(4.dp)
+                        .width(350.dp),shape = CircleShape,/*border = BorderStroke(2.dp,color = Color.White)*/
+                    ) {
+                        Text(text = "Join Room", modifier = Modifier.padding(2.dp),color=Color.White)
+
+                    }
+
                 }
 
-                FloatingActionButton(onClick = { navController.navigate(NavScreens.Room.route) },Modifier.size(120.dp,120.dp),shape = MaterialTheme.shapes.medium,backgroundColor = MaterialTheme.colors.primary) {
-                    Text(text = "Join Room",color = Color.White)
-                }
-                
             }
+
+
+
+
 
             Row(modifier = Modifier
                 .fillMaxWidth()
                 .offset(y = 70.dp), verticalAlignment = Alignment.CenterVertically) {
                 Column(modifier = Modifier.fillMaxWidth(),horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(text = "Create Raffles to help raise funds for your event.",
-                        modifier = Modifier.width(325.dp),textAlign = TextAlign.Center,fontFamily = /*PressStart*/FontFamily.Default,fontWeight = FontWeight.Bold,fontSize = 20.sp)
+//                    Text(text = "Create Raffles to help raise funds for your event.",
+//                        modifier = Modifier.width(325.dp),textAlign = TextAlign.Center,fontFamily = /*PressStart*/FontFamily.Default,fontWeight = FontWeight.Bold,fontSize = 20.sp)
                     Spacer(modifier = Modifier.height(100.dp))
                     Text(text = "Pixel Raffle is not liable for loss due to miss allocation of prizes or disputes pertaining to such matters. Please play fairly.",
                         modifier = Modifier.width(300.dp),textAlign = TextAlign.Justify,fontFamily = FontFamily.Default,fontWeight = FontWeight.Light,fontSize = 12.sp)
