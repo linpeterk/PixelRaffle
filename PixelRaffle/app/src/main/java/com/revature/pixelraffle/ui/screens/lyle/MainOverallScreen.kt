ef9d32e0bff7bcf96e27fa2b4548e5130670a0bc
Lyle <lylelopez03@gmail.com>
2022-04-28 11:49:34 -0700

    pic on main screen

--- PixelRaffle/app/src/main/java/com/revature/pixelraffle/ui/screens/lyle/MainOverallScreen.kt
+++ PixelRaffle/app/src/main/java/com/revature/pixelraffle/ui/screens/lyle/MainOverallScreen.kt
@@ -1,5 +1,6 @@
 package com.revature.pixelraffle.ui.screens.lyle
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
@@ -7,10 +8,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.revature.pixelraffle.R
 import com.revature.pixelraffle.ui.components.Logo
 import com.revature.pixelraffle.ui.navigation.NavScreens
 import com.revature.pixelraffle.ui.theme.PressStart
@@ -37,14 +41,13 @@ fun MainOverallScreen (navController: NavController){
 
     Scaffold(modifier = Modifier.fillMaxSize()/*,color= Color.White*/) {
 
-      //  Image(painter = painterResource(id = com.example.pixelraffle.R.drawable.mnbase_02), contentDescription = "",alpha = .25f,contentScale = ContentScale.FillBounds)
-
+        Image(painter = painterResource(id = R.drawable.mnbase_02), contentDescription ="",alpha = .20f,contentScale = ContentScale.FillBounds )
 
         Logo()
 
         Column(modifier = Modifier
             .fillMaxSize()
-            .offset(y = 200.dp)/*.border(2.dp,color= Color.Red)*/
+            .offset(y = 210.dp)/*.border(2.dp,color= Color.Red)*/
             .padding(4.dp),verticalArrangement = Arrangement.Center,horizontalAlignment = Alignment.CenterHorizontally) {
 
             Text(text = "ADDING THAT LITTLE EXTRA TO YOUR EVENT!",textAlign = TextAlign.Center, modifier = Modifier
